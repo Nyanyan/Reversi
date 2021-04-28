02aa2f7c6e64bd1a1bf162982480e1ceaf505180
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2021-04-29 03:51:03 +0900

    update

--- ai.py
+++ ai.py
@@ -1,6 +1,7 @@
 # Reversi AI
 
 import sys
+from random import random, shuffle
 def debug(*args, end='\n'): print(*args, file=sys.stderr, end=end)
 
 hw = 8
@@ -70,52 +71,71 @@ def check(grid, player, y, x):
                 res_grid[nny][nnx] = True
     return res, res_grid
 
-def calc_score(ty, tx, player, grid, depth):
+def calc_score(player, grid, depth, flag):
     if depth == 0:
-        return weight[ty][tx]
-    grid_copy = [[i for i in j] for j in grid]
-    _, plus_grid = check(grid_copy, player, ty, tx)
-    for y in range(hw):
-        for x in range(hw):
-            if plus_grid[y][x]:
-                grid_copy[y][x] = player
+        return 0
     res = 0
     for y in range(hw):
         for x in range(hw):
-            if grid_copy[y][x] == 2:
-                grid_copy[y][x] = -1
-                for dr in range(8):
-                    ny = y + dy[dr]
-                    nx = x + dx[dr]
-                    if not inside(ny, nx):
-                        res += depth + 1
-                    elif not empty(grid_copy, ny, nx):
-                        res += (depth + 1) * weight[ny][nx]
-    for y in range(hw):
-        for x in range(hw):
-            if not empty(grid_copy, y, x):
+            if grid[y][x] != 2:
                 continue
-            num = check(grid_copy, 1 - player, y, x)[0]
-            if num:
-                plus = (depth + 1) * weight[y][x]
-                if player == ai_player:
-                    res += plus
+            for dr in range(8):
+                ny = y + dy[dr]
+                nx = x + dx[dr]
+                if not inside(ny, nx):
+                    res += depth + 1
+                elif not empty(grid, ny, nx):
+                    res += (depth + 1) * weight[ny][nx]
+            res += (depth + 1) * weight[y][x] * 100
+            grid[y][x] = player
+            lst = []
+            for ny in range(hw):
+                for nx in range(hw):
+                    num, plus_grid = check(grid, 1 - player, ny, nx)
+                    if num:
+                        lst.append([ny, nx])
+            shuffle(lst)
+            for ny, nx in lst[:min(len(lst), 3)]:
+                grid_copy = [[i for i in j] for j in grid]
+                for nny in range(hw):
+                    for nnx in range(hw):
+                        if plus_grid[nny][nnx]:
+                            grid_copy[nny][nnx] = 1 - player
+                res -= calc_score(1 - player, grid_copy, depth - 1, False)
+            if not lst:
+                if flag:
+                    cnts = [0, 0]
+                    for nny in range(hw):
+                        for nnx in range(hw):
+                            if empty(grid, nny, nnx):
+                                continue
+                            cnts[grid[nny][nnx]] += 1
+                    if cnts[player] > cnts[1 - player]:
+                        res += 100000000
+                    else:
+                        res -= 100000000
                 else:
-                    res -= plus
-                res -= calc_score(y, x, 1 - player, grid_copy, depth - 1)
+                    res += calc_score(player, grid, depth - 1, True)
+            grid[y][x] = -1
     return res
 
-max_depth = 3
+max_depth = 2
 ai_player = int(input())
 grid = [[int(i) for i in input().split()] for _ in range(hw)]
-max_score = -10000000
+max_score = -100000000000000
 final_y = -1
 final_x = -1
 for y in range(hw):
     for x in range(hw):
         if grid[y][x] != 2:
             continue
-        score = calc_score(y, x, ai_player, grid, max_depth) + weight[y][x] * (max_depth ** 2)
+        grid_copy = [[i for i in j] for j in grid]
+        num, plus_grid = check(grid, ai_player, y, x)
+        for ny in range(hw):
+            for nx in range(hw):
+                if plus_grid[ny][nx]:
+                    grid_copy[ny][nx] = ai_player
+        score = calc_score(ai_player, grid_copy, max_depth, False) + weight[y][x] * (max_depth + 1)
         debug('ai debug', y, x, score)
         if max_score < score:
             max_score = score
